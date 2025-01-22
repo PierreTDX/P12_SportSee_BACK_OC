261515f73762f2b01b91785279cea977cab006b4
PierreTDX <pierre.tondeux@gmail.com>
2025-01-22 17:25:56 +0100

    config .env

--- app/index.js
+++ app/index.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const cors = require('cors')
+require('dotenv').config(); // Charger les variables d'environnement
 
-const router = require('./routes')
+const express = require('express');
+const cors = require('cors');
+const router = require('./routes');
 
-const app = express()
-app.use(cors())
-const port = 3000
+const app = express();
+app.use(cors());
 
-app.use(router)
+// Utilisez les variables d'environnement avec une valeur par défaut
+const port = process.env.PORT || 3000;
+const baseUrl = process.env.BASE_URL || `http://localhost:${port}`;
 
-app.listen(port, () => console.log(`Magic happens on port ${port}`))
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Magic happens on ${baseUrl}`);
+});
