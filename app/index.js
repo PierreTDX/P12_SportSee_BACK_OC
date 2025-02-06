bc00f441c50c8c820a6ace245ba1e10c18f489e5
PierreTDX <pierre.tondeux@gmail.com>
2025-02-06 10:22:10 +0100

    Texte d'information : en listes

--- app/index.js
+++ app/index.js
@@ -22,19 +22,19 @@ app.get('/', (req, res) => {
     '<h1>Possible endpoints</h1>' +
     '<p>This project includes four endpoints that you will be able to use:</p>' +
     '<ul>' +
-    '<p><b>http://localhost:3000/user/${userId}</b> - retrieves information from a user. This first endpoint includes the user id, user information (first name, last name and age), the current day\'s score (todayScore) and key data (calorie, macronutrient, etc.).</p>' +
-    '<p><b>http://localhost:3000/user/${userId}/activity</b> - retrieves a user\'s activity day by day with kilograms and calories.</p>' +
-    '<p><b>http://localhost:3000/user/${userId}/average-sessions</b> - retrieves the average sessions of a user per day. The week starts on Monday.</p>' +
-    '<p><b>http://localhost:3000/user/${userId}/performance</b> - retrieves a user\'s performance (energy, endurance, etc.).</p>' +
+    '<li><b>http://localhost:3000/user/${userId}</b> - retrieves information from a user. This first endpoint includes the user id, user information (first name, last name and age), the current day\'s score (todayScore) and key data (calorie, macronutrient, etc.).</li>' +
+    '<li><b>http://localhost:3000/user/${userId}/activity</b> - retrieves a user\'s activity day by day with kilograms and calories.</li>' +
+    '<li><b>http://localhost:3000/user/${userId}/average-sessions</b> - retrieves the average sessions of a user per day. The week starts on Monday.</li>' +
+    '<li><b>http://localhost:3000/user/${userId}/performance</b> - retrieves a user\'s performance (energy, endurance, etc.).</li>' +
     '</ul>' +
     '<p>Warning, currently only two users have been mocked. They have userId 12 and 18 respectively.</p>' +
     '<h1>Examples of queries</h1>' +
     '<ul>' +
-    '<p><b>http://localhost:3000/user/12/performance</b> - Retrieves the performance of the user with id 12</p>' +
-    '<p><b>http://localhost:3000/user/18</b> - Retrieves user 18\'s main information.</p>' +
+    '<li><b>http://localhost:3000/user/12/performance</b> - Retrieves the performance of the user with id 12</li>' +
+    '<li><b>http://localhost:3000/user/18</b> - Retrieves user 18\'s main information.</li>' +
     '</ul>'+
     '<br>' +
-    '<p>Warning, if you use the online API, change http://localhost:3000 by https://p12-sport-see-back-oc.vercel.app</p>'
+    '<h4>Warning, if you use the online API, change <u>http://localhost:3000</u> by <u>https://p12-sport-see-back-oc.vercel.app</u></h4>'
     );
   });
 
